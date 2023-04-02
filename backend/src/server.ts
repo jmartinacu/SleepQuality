39ed8d6a75a1f3d36d7fee6f7cc3f0c29febcc4b
Joaquin Martin <jmartinacu2002@gmail.com>
2023-04-02 22:40:22 +0200

    Fix linter for typescript

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,19 @@
-console.log('server.js')
+import fastify from 'fastify'
+
+const server = fastify()
+
+server.get('/', async (_request, _reply) => {
+  return { message: 'Sleep Quality API' }
+})
+
+server.get('/healthcheck', async (_request, _reply) => {
+  return { status: 'ok' }
+})
+
+server.listen({ port: 8080 }, (err, address) => {
+  if (err != null) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log(`Server listening at ${address}`)
+})
