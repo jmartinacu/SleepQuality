f4cb077206412bef316c0637a2aa0f1046a1f9aa
Joaquin Martin <jmartinacu2002@gmail.com>
2023-04-03 14:09:18 +0200

    Api Dir structure defined

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import fastify from 'fastify'
 
-const start = async () => {
+const start = async (): Promise<null> => {
   try {
     if (process.env.NODE_ENV !== 'production') {
       const path = await import('node:path')
@@ -23,13 +23,15 @@ const start = async () => {
 
     await server.listen({ port: PORT })
 
-    const {port, address} = server.addresses()[0]
+    const { port, address } = server.addresses()[0]
 
     console.log(`Server listening at http://${address}:${port}`)
+
+    return null
   } catch (e) {
     console.error(e)
     process.exit(1)
   }
 }
 
-start()
+void start()
